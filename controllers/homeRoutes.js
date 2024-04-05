a8075c80d77f0273cf0df0afab792767bf5204c7
Nico P <nicopetkas@gmail.com>
2024-04-05 10:41:02 -0700

    notes update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all posts and JOIN with user data
+    // Get all posts
     const postData = await Post.findAll({
       include: [
         {
@@ -14,10 +14,10 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
+    // Serialize data 
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
+    
     res.render('homepage', { 
       posts, 
       logged_in: req.session.logged_in 
@@ -53,10 +53,10 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
+    // Finds logged in user based on the session ID
 router.get('/dashboard', withAuth, async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
+
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: Post }],
@@ -74,7 +74,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
+  // If user logged in, redirect request to another route
   if (req.session.logged_in) {
     res.redirect('/');
     return;
